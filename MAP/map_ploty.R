db8788d520cb28bc2a43a1fb4475a9fed3e43b64
Julia G. de Aledo <75074514+juliagdealedo@users.noreply.github.com>
2025-09-11 16:09:38 +0200

    R script with an example

---
+++ MAP/map_ploty.R
@@ -0,0 +1,26 @@
+library(plotly)
+df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+
+df$q <- with(df, cut(pop, quantile(pop)))
+levels(df$q) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
+df$q <- as.ordered(df$q)
+
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showland = TRUE,
+  landcolor = toRGB("gray85"),
+  subunitwidth = 1,
+  countrywidth = 1,
+  subunitcolor = toRGB("white"),
+  countrycolor = toRGB("white")
+)
+
+fig <- plot_geo(df, locationmode = 'USA-states', sizes = c(1, 250))
+fig <- fig %>% add_markers(
+  x = ~lon, y = ~lat, size = ~pop, color = ~q, hoverinfo = "text",
+  text = ~paste(df$name, "<br />", df$pop/1e6, " million")
+)
+fig <- fig %>% layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
+
+fig
