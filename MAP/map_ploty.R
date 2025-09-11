0294966e476c24223ff480aa3b95c530edd50b05
Jimena Mateo <jimena.mateo.martin@gmail.com>
2025-09-11 18:34:34 +0200

    first map attempt

--- MAP/map_ploty.R
+++ MAP/map_ploty.R
@@ -24,3 +24,36 @@ fig <- fig %>% add_markers(
 fig <- fig %>% layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
 
 fig
+
+
+
+
+library(rnaturalearth)
+world_data <- ne_countries(scale = "medium", returnclass = "sf")
+world_contour <- world_data$geometry
+
+#random data
+researchers <- c("Julia", "Zelda", "Munai", "Osiris", "Jimena")
+countries <- c("Spain", "Japan", "Peru", "Egypt", "Nepal")
+df <- data.frame(researcher = researchers, country = countries)
+
+# check if countries in df are in world_data$name
+countries %in% world_data$name
+
+library(ggplot2)
+library(dplyr)
+library(sf)
+
+selected_countries <- world_data %>%
+  filter(name %in% df$country) %>%
+  left_join(df, by = c("name" = "country")) %>%
+  mutate(hover_text = paste("Country:", name, "<br>Researcher:", researcher))
+
+g <- ggplot() +
+  geom_sf(data = world_data, fill = NA, color = "#a9a9a9") +
+  geom_sf(data = selected_countries, aes(fill = name, text = hover_text), color = "#a9a9a9") +
+  scale_fill_viridis_d() +
+  theme_minimal()
+
+p <- ggplotly(g, tooltip = "text")
+htmlwidgets::saveWidget(p, "mapa_interactivo.html")
